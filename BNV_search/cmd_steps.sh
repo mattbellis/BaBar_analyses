22119cd40be274e4fdef616e0154b0d659df48e9
mattbellis <matthew.bellis@gmail.com>
2019-12-27 02:43:54 -0500

    Stuff

---
+++ BNV_search/cmd_steps.sh
@@ -0,0 +1,9 @@
+#python dump_ROOT_files_based_on_PID_assignments.py /home/bellis/babar_data/basicPID_pi0_R24/SP-11976-R24-1_SKIMMED.root
+#python look_at_kinematic_distributions.py /home/bellis/babar_data/basicPID_pi0_R24/SP-11976-R24-1_SKIMMED_PID_skim.root
+#python make_plots_for_talk.py /home/bellis/babar_data/basicPID_R24-SP-11975-R24-1_PID_skim_KINVARS_pnu.root
+
+python dump_ROOT_files_based_on_PID_assignments.py /home/bellis/babar_data/basicPID_pi0_R24/SP-1237-R24-1_SKIMMED.root
+python look_at_kinematic_distributions.py /home/bellis/babar_data/basicPID_pi0_R24/SP-1237-R24-1_SKIMMED_PID_skim.root
+
+python plot_KINVAR_files_missing_particle.py ~/babar_data/basicPID_pi0_R24/SP-11975-R24-1_SKIMMED_PID_skim_KINVARS_pnu.root
+
