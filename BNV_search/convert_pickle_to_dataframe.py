8daccca4896fdc740f17ddebb4a576500ea55683
Matt Bellis <matthew.bellis@gmail.com>
2020-09-08 00:29:41 -0400

    Stuff

--- BNV_search/convert_pickle_to_dataframe.py
+++ BNV_search/convert_pickle_to_dataframe.py
@@ -11,12 +11,29 @@ import pandas as pd
 
 infilenames = sys.argv[1:]
 allvars,histos = bt.read_in_files_and_combine_all_the_dictionaries(infilenames)
-df = pd.DataFrame.from_dict(allvars)
 
+# What if we only want from cut 1 on when nbnvbcand == 1?
+x = allvars['nbnvbcand']['values'][3] 
+idx = x==1
+
+tmp = {}
+for key in allvars.keys():
+
+    # Skip the PID flags
+    if key.find('Is')>=0:
+        continue
+    print(key,len(allvars[key]['values'][3]),len(idx))
+    tmp[key] = allvars[key]['values'][3][idx]
+
+df = pd.DataFrame.from_dict(tmp)
+
+
+#df = pd.DataFrame.from_dict(allvars)
+#
 sp = infilenames[0].split('SP-')[1].split('/')[0]
 decay = infilenames[0].split('SP-')[1].split('/')[1]
 outname = 'CUT_SUMMARY_SP-{0}_{1}.h5'.format(sp,decay)
-
+#
 df.to_hdf(outname,key='df',mode='w')
 
 # Can do stuff like
