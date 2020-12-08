9d2330510e8285ec957447959970387d58c84fcb
Matt Bellis <matthew.bellis@gmail.com>
2020-12-08 12:48:34 -0500

    Stuff

--- BNV_search/merge_a_bunch_of_dataframes.py
+++ BNV_search/merge_a_bunch_of_dataframes.py
@@ -1,6 +1,8 @@
 import sys
 import pandas as pd
 
+import babar_dataframe_tools as bd
+
 #outfilename = sys.argv[1]
 
 infilenames = sys.argv[1:]
@@ -13,9 +15,14 @@ if infilenames[0].find('SP-')>=0:
     decay = infilenames[0].split('SP-')[1].split('/')[1]
     outfilename = 'CUT_SUMMARY_SP-{0}_{1}.h5'.format(sp,decay)
 elif infilenames[0].find('AllEvents-')>=0:
-    sp = infilenames[0].split('AllEvents-')[1].split('-')[0]
-    decay = infilenames[0].split('AllEvents')[1].split('/')[1]
-    outfilename = 'CUT_SUMMARY_AllEvents-{0}_{1}.h5'.format(sp,decay)
+    #sp = infilenames[0].split('AllEvents-')[1].split('-')[0]
+    #decay = infilenames[0].split('AllEvents')[1].split('/')[1]
+    #outfilename = 'CUT_SUMMARY_AllEvents-{0}_{1}.h5'.format(sp,decay)
+
+    # To merge all the data files
+    decay = infilenames[0].split('_')[-1].split('.')[0]
+    #outfilename = 'CUT_SUMMARY_AllEvents-MERGED_{0}_{1}.h5'.format('AllRuns',decay)
+    outfilename = 'CUT_SUMMARY_AllEvents-MERGED_{0}_WITHBNVCHILDRENPCUT_{1}.h5'.format('AllRuns',decay)
 
 print(outfilename)
 #exit()
@@ -26,10 +33,16 @@ for i,infilename in enumerate(infilenames):
 
     if i%100==0:
         print(i,nfiles,infilename)
+    print(i,nfiles,infilename)
 
     df = pd.read_hdf(infilename)
 
-    frames.append(df)
+
+    #bnv_children_momentum_mask = bd.bnv_children_momentum_mask(df,child='proton') & bd.bnv_children_momentum_mask(df,child='electron')
+    bnv_children_momentum_mask = bd.bnv_children_momentum_mask(df,child='electron')
+
+
+    frames.append(df[bnv_children_momentum_mask])
 
 
 df_merged = pd.concat(frames)
