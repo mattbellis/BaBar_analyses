f0e30c88b54bf133d917ae9ce4a3c402badd96ba
Matt Bellis <matthew.bellis@gmail.com>
2021-03-29 02:41:55 -0400

    Stuff

--- BNV_search/pdf_definitions.py
+++ BNV_search/pdf_definitions.py
@@ -19,3 +19,25 @@ def argus_in_x(x,tag='default'):
     return pars, argus 
 ################################################################################
 
+################################################################################
+# Two Argus
+################################################################################
+def two_argus_in_x(x,tag='default'):
+    argpar0 = ROOT.RooRealVar("argpar0_"+tag,"Argus shape par "+tag,-20.0,-1000,10000)
+    cutoff0 = ROOT.RooRealVar("cutoff0_"+tag,"Argus cutoff "+tag,1.00,-1000,1000)
+    argus0 = ROOT.RooArgusBG("argus_"+tag,"Argus PDF "+tag,x,cutoff0,argpar0)
+
+    argpar1 = ROOT.RooRealVar("argpar1_"+tag,"Argus shape par "+tag,-20.0,-1000,10000)
+    cutoff1 = ROOT.RooRealVar("cutoff1_"+tag,"Argus cutoff "+tag,1.00,-1000,1000)
+    argus1 = ROOT.RooArgusBG("argus_"+tag,"Argus PDF "+tag,x,cutoff1,argpar1)
+
+    frac_two_argus = ROOT.RooRealVar("argus_frac_"+tag,"Two Argus fraction "+tag,0.50,0,1.0)
+
+    twoArgus = ROOT.RooAddPdf(
+    "twoArgus_"+tag, "Two Argus functions "+tag, ROOT.RooArgList(argus0, argus1), ROOT.RooArgList(frac_two_argus))
+
+    pars = [argpar0, cutoff0, argpar1, cutoff1, frac]
+
+    return pars, twoArgus, argus0, argus1
+################################################################################
+
