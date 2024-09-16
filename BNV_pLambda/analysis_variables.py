baddffd5872da94f08ccf1eaaa4fdc2043456843
Josephine Swann <jr21swan@siena.edu>
2024-09-16 12:30:17 -0400

    minor changes, automation

---
+++ BNV_pLambda/analysis_variables.py
@@ -0,0 +1,9 @@
+region_definitions = {}
+region_definitions['Area'] = ['Fitting area', 'Blinding area']
+# Low and then high
+region_definitions['blinding MES'] = [5.27, 5.3]
+region_definitions['fitting MES'] = [5.2, 5.3]
+
+region_definitions['blinding DeltaE'] = [-0.07, 0.07]
+region_definitions['fitting DeltaE'] = [-0.2, 0.2]
+
