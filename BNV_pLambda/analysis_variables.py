46a6c7319edb74cd29884aaf31ff72cbfce84515
mattbellis <matthew.bellis@gmail.com>
2024-11-05 15:36:18 -0500

    Minor changes to analysis variables

--- BNV_pLambda/analysis_variables.py
+++ BNV_pLambda/analysis_variables.py
@@ -1,10 +1,10 @@
 region_definitions = {}
-region_definitions['Area'] = ['Fitting area', 'Blinding area']
+region_definitions['Area'] = ['Fitting area', 'Signal area']
 # Low and then high
-region_definitions['blinding MES'] = [5.27, 5.3]
+region_definitions['signal MES'] = [5.27, 5.3]
 region_definitions['fitting MES'] = [5.2, 5.3]
 
-region_definitions['blinding DeltaE'] = [-0.07, 0.07]
+region_definitions['signal DeltaE'] = [-0.07, 0.07]
 region_definitions['fitting DeltaE'] = [-0.2, 0.2]
 
 region_definitions['sideband 1 DeltaE'] = [0.07, 0.14]
