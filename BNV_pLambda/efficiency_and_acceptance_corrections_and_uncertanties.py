8c7bd4f17de05e4d7eb7dad22c0e10a679436aa9
mattbellis <matthew.bellis@gmail.com>
2024-12-02 20:34:22 -0500

    Some changes

---
+++ BNV_pLambda/efficiency_and_acceptance_corrections_and_uncertanties.py
@@ -0,0 +1,10 @@
+signal_efficiency_terms = {}
+
+# All terms are the efficiency of the cut on signal. 
+# * The fraction of signal remaining after the cut. 
+# * The statistical uncertainty
+# * The systematic uncertainty
+
+# Make sure we put which file is doing the calculation
+# XXXXXXXXXXXXXXXXXXXXXXXXXXXXXx
+signal_efficiency_terms['Lambda0 flight len and mass cut'] = [0.85, 0.02, 0.02]
